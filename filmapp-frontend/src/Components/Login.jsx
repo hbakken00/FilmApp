f6645d50f3c34faa46305888b379387e1d232f83
hbakken00 <nikhaug@outlook.com>
2024-05-16 19:25:16 +0200

    Login komponent

---
+++ filmapp-frontend/src/Components/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import users from "../../../filmapp-backend/schemaTypes/brukere"
+
+// login "velg bruker funksjon"
+const VelgBruker = ({ onUserSelect }) => {
+    return (
+
+
+
+    )  // ikke ferdig 
+} ; 
