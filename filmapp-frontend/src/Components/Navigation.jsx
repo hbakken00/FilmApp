b1a64d4131b9525c53a181c0f10e17a1d8ad4b71
hbakken00 <nikhaug@outlook.com>
2024-05-19 00:19:54 +0200

    links

--- filmapp-frontend/src/Components/Navigation.jsx
+++ filmapp-frontend/src/Components/Navigation.jsx
@@ -7,9 +7,9 @@ export const Navigation = () =>{
     <header id="navheader">
         <nav className="nav">
             <ul className="navul">
-                <li><a href="#"> <RiTv2Line /> Hva skal jeg se?</a></li>
-                <li><a href="#"> Bla gjennom sjangere</a></li>
-                <li><a href="#"> <RiAccountCircleFill />  Bruker</a></li>
+          <li><Link to="/home"><RiTv2Line /> Hva skal jeg se?</Link></li>
+          <li><Link to="/genres">Bla gjennom sjangere</Link></li>
+          <li><Link to="/select-user"><RiAccountCircleFill /> Bruker</Link></li>
             </ul>
         </nav>
     </header>
