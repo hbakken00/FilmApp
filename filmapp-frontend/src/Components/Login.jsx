8624302d7b32dfe806da0413446bf571af255322
hbakken00 <nikhaug@outlook.com>
2024-05-18 16:24:09 +0200

    Update

--- filmapp-frontend/src/Components/Login.jsx
+++ filmapp-frontend/src/Components/Login.jsx
@@ -11,12 +11,12 @@ const VelgBruker = ({ onUserSelect }) => {
         </header>
         <nav>
          <ul className="bruker-knapper">  
-        {users.map(user => ( // mapper brukere fra users i Sanity og generer knapper for hver bruker. Onclick med 
+        {users.map(user => ( // mapper brukere fra users.js arrayen i Sanity og generer knapper for hver bruker. Onclick med 
             <li key={user.id}>
               <button onClick={() => onUserSelect(user)}>{user.name}</button>
            </li>
          ))}
-         
+
          </ul> 
         </nav>
       </section>
@@ -24,3 +24,5 @@ const VelgBruker = ({ onUserSelect }) => {
 } 
 
 export default VelgBruker
+
+// har forsøkt å gjøre dette så semantisk som mulig med section for innholdskropp, header, nav og ul <li> 
