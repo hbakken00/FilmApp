3294ad73e70766a451cd94a27e2e216af7327009
hbakken00 <nikhaug@outlook.com>
2024-05-18 16:22:25 +0200

    Update på Login.jsx

--- filmapp-frontend/src/Components/Login.jsx
+++ filmapp-frontend/src/Components/Login.jsx
@@ -9,17 +9,16 @@ const VelgBruker = ({ onUserSelect }) => {
           <h1>Hvem skal se idag?</h1>
           <p>Velg bruker</p>
         </header>
-        
+        <nav>
+         <ul className="bruker-knapper">  
         {users.map(user => ( // mapper brukere fra users i Sanity og generer knapper for hver bruker. Onclick med 
             <li key={user.id}>
-              <button onClick={() => onUserSelect(user)}>  
-                {user.name}
-              </button>
-            </li>
-    ))}
-
-
-
+              <button onClick={() => onUserSelect(user)}>{user.name}</button>
+           </li>
+         ))}
+         
+         </ul> 
+        </nav>
       </section>
     )  // ikke ferdig 
 } 
