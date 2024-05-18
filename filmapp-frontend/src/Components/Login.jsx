90d05a9ba34c54b1c38f622c1aae8d5ce49f67a8
hbakken00 <nikhaug@outlook.com>
2024-05-18 16:01:57 +0200

    Update Login.jsx

--- filmapp-frontend/src/Components/Login.jsx
+++ filmapp-frontend/src/Components/Login.jsx
@@ -2,16 +2,26 @@ import React from "react"
 import users from "../../../filmapp-backend/schemaTypes/brukere"
 
 // login "velg bruker funksjon"
-const VelgBruker = ({ onUserSelect }) => {
+const VelgBruker = ({ onUserSelect }) => { 
     return (
       <section className="bruker-valg">
         <header>
           <h1>Hvem skal se idag?</h1>
           <p>Velg bruker</p>
         </header>
+        
+        {users.map(user => ( // mapper brukere fra users i Sanity og generer knapper for hver bruker. Onclick med 
+            <li key={user.id}>
+              <button onClick={() => onUserSelect(user)}>  
+                {user.name}
+              </button>
+            </li>
+    ))}
 
 
 
       </section>
-    );  // ikke ferdig 
-} ; 
+    )  // ikke ferdig 
+} 
+
+export default VelgBruker
