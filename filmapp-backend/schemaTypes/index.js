7b8b671583a86d39464db3cf8d87cc8fab64efd5
hbakken00 <nikhaug@outlook.com>
2024-05-19 15:59:20 +0200

    schema fikset på index.js backend

--- filmapp-backend/schemaTypes/index.js
+++ filmapp-backend/schemaTypes/index.js
@@ -2,5 +2,8 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import users from './users.js'
+import movie from "./movie.js"
+import genre from './genre.js'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, blockContent, users, movie, genre ]
--- filmapp-backend/schemaTypes/users.js
+++ filmapp-backend/schemaTypes/users.js
@@ -1,4 +1,5 @@
 
+
 export default {
   name: 'user',
   title: 'User',
