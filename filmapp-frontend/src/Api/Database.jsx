f23ebfcdf2e45231e02d9ed6be6873d6c4e9a9ec
naathljee <146845810+naathljee@users.noreply.github.com>
2024-05-19 15:11:33 +0200

    prøver å fetche med axios - Database.jsx

--- filmapp-frontend/src/Api/Database.jsx
+++ filmapp-frontend/src/Api/Database.jsx
@@ -1,25 +1,22 @@
 function film_data({}){
     const fetchData =async () => {
         
-            const fetch = require('node-fetch');
+        const axios = require('axios');
 
-            const url = 'https://moviesdatabase.p.rapidapi.com/titles/series/%7BseriesId%7D';
-            const options = {
-              method: 'GET',
-              headers: {
-                'X-RapidAPI-Key': 'c68fd6996amsh6da0d7a3ede5ab6p16be71jsn3a48a3384c43',
-                'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-            }}
-            
-            
-            try {
-                const response = await fetch(url, options);
-                const result = await response.text();
-                console.log(result);
-            } catch (error) {
-                console.error(error);
-            }
+        const options = {
+          method: 'GET',
+          url: 'https://moviesdatabase.p.rapidapi.com/titles/series/%7BseriesId%7D',
+          headers: {
+            'X-RapidAPI-Key': 'c68fd6996amsh6da0d7a3ede5ab6p16be71jsn3a48a3384c43',
+            'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+          }
+        };
         
-
+        try {
+            const response = await axios.request(options);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
 }};
 export default film_data
