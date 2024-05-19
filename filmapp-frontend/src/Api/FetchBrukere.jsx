c52c76ba70d67fc7a0da76a96fd7724e094faa50
hbakken00 <nikhaug@outlook.com>
2024-05-19 16:59:11 +0200

    Endringer i schemas og la til fetch brukere

--- filmapp-frontend/src/Api/FetchBrukere.jsx
+++ filmapp-frontend/src/Api/FetchBrukere.jsx
@@ -1,1 +1,56 @@
 
+
+import React, { useEffect, useState } from "react";
+import sanityClient from "./sanityClient"
+import PropTypes from 'prop-types'
+import Login from "../Components/Login"
+
+const query = `*[_type == "user"]{
+  _id,
+  name,
+  username,
+  "favoriteMovies": favoriteMovies[]->{
+    _id,
+    title,
+    imdb_id,
+    cover_image
+  },
+  "preferredGenres": preferredGenres[]->{
+    _id,
+    name
+  }
+}`;
+
+const FetchUsers = ({ onUserSelect }) => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const data = await sanityClient.fetch(query);
+        setUsers(data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+        setError("Failed to load users.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) return <p>Laster inn ...</p>;
+  if (error) return <p>{error}</p>;
+  if (!users.length) return <p>Ingen brukere tilgjengelige</p>;
+
+  return <Login onUserSelect={onUserSelect} users={users} />;
+}
+
+FetchUsers.propTypes = { // måtte legge inn denne fordi proptype error i konsollen 
+    onUserSelect: PropTypes.func.isRequired, 
+  }
+  
+export default FetchUsers;
