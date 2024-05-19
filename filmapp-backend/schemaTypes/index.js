042ae0e0acab71deb323798a0e68fccdfaf16349
hbakken00 <nikhaug@outlook.com>
2024-05-19 17:24:48 +0200

    endret index schema

--- filmapp-backend/schemaTypes/index.js
+++ filmapp-backend/schemaTypes/index.js
@@ -1,9 +1,7 @@
-import blockContent from './blockContent'
+
 import category from './category'
-import post from './post'
-import author from './author'
 import users from './users.js'
 import movie from "./movie.js"
 import genre from './genre.js'
 
-export const schemaTypes = [post, author, category, blockContent, users, movie, genre ]
+export const schemaTypes = [category, users, movie, genre ]
