11d373addc399b82bec73f5ce44a67c6b2abf948
Theasfo <thea.sofie.forrisdahl@gmail.com>
2024-05-20 11:49:13 +0200

    Endringer i api og movicard

--- filmapp-frontend/src/Components/MovieCard.jsx
+++ filmapp-frontend/src/Components/MovieCard.jsx
@@ -1,65 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
-const filmDatabase = async () => {
-    const url = 'https://imdb188.p.rapidapi.com/api/v1/searchIMDB?query=%3CREQUIRED%3E';
+//api for moviecard
+export const filmDatabase = async (query) => {
     const options = {
         method: 'GET',
+        url: 'https://imdb188.p.rapidapi.com/api/v1/searchIMDB?query=%3CREQUIRED%3E',
+        params: {q: query},
         headers: {
             'X-RapidAPI-Key': 'c68fd6996amsh6da0d7a3ede5ab6p16be71jsn3a48a3384c43',
             'X-RapidAPI-Host': 'imdb188.p.rapidapi.com'
         }
     };
 
-    try {
-        const response = await fetch(url, options);
-        if (!response.ok) {
-            throw new Error(`Error! status: ${response.status}`);
-        }
-        const result = await response.json();
-        return result.movies || []; 
-    } catch (error) {
+    
+
+    try{
+        const response = await axios.request(options);
+        return response.data;
+    } catch (error){
         console.error(error);
-        throw error;
     }
+
 };
 
-const MovieCard = () => {
-    const [movies, setMovies] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+//moviecard componente
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const movies = await filmDatabase();
-                setMovies(movies);
-            } catch (error) {
-                setError("Failed to fetch the data");
-            } finally {
-                setLoading(false);
-            }
-        };
+const MovieCard = ({movie}) => {
+    return(
+        <section className="Movie-card">
+            <h3>{movie}</h3>
+            <a href={'https://www.imdb.com/title/${movie.imdb_id}'}target="_blank" rel="noopener noreferrer">Mer om filmen</a>
+        </section>
+    )
+}
 
-        fetchData();
-    }, []);
-
-    if (loading) return <p>Loading, please wait...</p>;
-    if (error) return <p>{error}</p>;
-    if (!movies.length) return <p>No Movies, sorry :/</p>;
-
-    return (
-        <article>
-            {movies.map(movie => (
-                <section key={movie.imdb_id}>
-                    <h2>{movie.title}</h2>
-                    <img src={movie.cover_image} alt={`${movie.title} poster`} />
-                    <p>Genres: {movie.genres.map((genre) => genre.name).join(', ')}</p>
-                    <Link to={`https://www.imdb.com/title/${movie.imdb_id}`} target="_blank">View on IMDB</Link>
-                </section>
-            ))}
-        </article>
-    );
-};
-
-export default MovieCard;
+export default MovieCard
