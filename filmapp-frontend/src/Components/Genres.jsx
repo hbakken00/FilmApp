0a512cd63b1478ebbea3bce6f12db13ed8f171a3
naathljee <146845810+naathljee@users.noreply.github.com>
2024-05-20 20:33:55 +0200

    fikset error i konsollen

--- filmapp-frontend/src/Components/Genres.jsx
+++ filmapp-frontend/src/Components/Genres.jsx
@@ -1,57 +1,57 @@
 //API Fetching for sjanger.
-
-import { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
 
 const Genres = () => {
-    const [movieType,setMovieType] = useState([])
-    const [error,setError] = useState(null)
-    const [loading,setLoading] = useState(true)
-
+    const [movieType,setMovieType] = useState([]);
+    const [error,setError] = useState(null);
+    const [loading,setLoading] = useState(true);
+    
     useEffect(() => { 
-    const fetchGenres = async () => { 
-    const url = 'https://imdb188.p.rapidapi.com/api/v1/getGenres';
-    const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c68fd6996amsh6da0d7a3ede5ab6p16be71jsn3a48a3384c43',
-		'X-RapidAPI-Host': 'imdb188.p.rapidapi.com'
-	}
-};
+        const fetchGenres = async () => { 
+            const url = 'https://imdb188.p.rapidapi.com/api/v1/getGenres';
+            const options = {
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Key': 'c68fd6996amsh6da0d7a3ede5ab6p16be71jsn3a48a3384c43',
+                    'X-RapidAPI-Host': 'imdb188.p.rapidapi.com'
+                }
+            };
+            
+            try {
+                const response = await fetch(url, options);
+                const result = await response.text();
+                setMovieType(response.movieType.results || []);
+                console.log(result);
+                
+            } catch (error) {
+                setError("feil på sjanger siden", error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchGenres();
+        console.log(fetchGenres, "dette funker")    
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-    setMovieType(response.movieType.results || []);
-	console.log(result);
-
-} catch (error) {
-	setError("feil på sjanger siden", error);
-} finally {
-    setLoading(false);
-}
- }
- fetchGenres();
-}, []);
-
+    }, []);
+    
     if (loading)return <p>Loading Please Wait </p>
     if (error) return <p>{error}</p>
-
-
-return (
-    <section>
+    
+    
+    return (
+        <section>
         <h2>All Genres:</h2>
         <ul>
-            <li>
-                {movieType.map((genre, item)=> (
-                    <li key={item}>
-                        <p>{genre}</p> </li>
-                )
-                )}
+        <li>
+        {movieType.map((genre, item)=> (
+            <li key={item}>
+            <p>{genre}</p> </li>
+            )
+            )}
             </li>
-        </ul>
-    </section>
-)
+            </ul>
+            </section>
+            )
 }
-
-export default Genres()
+export default Genres;
